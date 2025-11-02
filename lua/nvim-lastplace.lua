fba52e77759f9a682f680dc3082b22f084c61ea7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-11-01 20:47:48 -0600

    refactor: split operations into functions for readable code
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/nvim-lastplace.lua
+++ lua/nvim-lastplace.lua
@@ -1,29 +1,28 @@
 ---@class Lastplace
 local Lastplace = {}
 
----@class Lastplace.Opts
----A tuple containing both lists of excluded filetypes and buftypes.
----
----Default:
---- - bt: `{ 'quickfix', 'nofile', 'help' }`
---- - ft: `{ 'gitcommit', 'gitrebase', 'svn', 'hgcommit' }`
----`
----@field ignore? { ft: string[], bt: string[] }
----If true it wil automatically open folds upon file reading.
----
----Default: `true`
----@field open_folds? boolean
-
 ---@type Lastplace.Opts
 Lastplace.options = {}
 
 ---@return Lastplace.Opts
 function Lastplace.get_defaults()
-    local opts = { ---@type Lastplace.Opts
-        ignore = {
+    ---@class Lastplace.Opts
+    local opts = {
+        ---A tuple containing both lists of excluded filetypes and buftypes.
+        ---
+        ---Default:
+        --- - bt: `{ 'quickfix', 'nofile', 'help' }`
+        --- - ft: `{ 'gitcommit', 'gitrebase', 'svn', 'hgcommit' }`
+        ---
+        ignore = { ---@type { ft: string[], bt: string[] }
             bt = { 'quickfix', 'nofile', 'help' },
             ft = { 'gitcommit', 'gitrebase', 'svn', 'hgcommit' },
         },
+        ---If true it wil automatically open folds upon file reading.
+        ---
+        ---Default: `true`
+        ---
+        ---@type boolean
         open_folds = true,
     }
     return opts
@@ -36,6 +35,7 @@ function Lastplace.setup(opts)
     else
         vim.validate({ opts = { opts, { 'table', 'nil' } } })
     end
+
     Lastplace.options = vim.tbl_deep_extend('keep', opts or {}, Lastplace.get_defaults())
 
     local group = vim.api.nvim_create_augroup('NvimLastplace', { clear = true })
@@ -53,26 +53,42 @@ function Lastplace.setup(opts)
     })
 end
 
+---Reset cursor to first line.
+---
+local function reset_to_top()
+    vim.api.nvim_command([[normal! gg]])
+end
+
+---Attempt to center the line in the buffer.
+---
+local function center_line()
+    vim.api.nvim_command([[normal! zvzz]])
+end
+
+---Sets line to last line edited.
+--
+local function set_to_last_place()
+    vim.api.nvim_command([[keepjumps normal! g`"]])
+end
+
 local function set_cursor_position()
-    local last_line = vim.fn.line([['"]])
-    local buff_last_line = vim.fn.line('$')
-    local window_last_line = vim.fn.line('w$')
-    local window_first_line = vim.fn.line('w0')
+    local last = vim.fn.line([['"]])
+    local buf_last = vim.fn.line('$')
+    local window = { first = vim.fn.line('w0'), last = vim.fn.line('w$') }
     -- If the last line is set and the less than the last line in the buffer
-    if last_line > 0 and last_line <= buff_last_line then
+    if last > 0 and last <= buf_last then
         -- Check if the last line of the buffer is the same as the window
-        if window_last_line == buff_last_line then
-            -- Set line to last line edited
-            vim.api.nvim_command([[keepjumps normal! g`"]])
-            -- Try to center
-        elseif buff_last_line - last_line > ((window_last_line - window_first_line) / 2) - 1 then
-            vim.api.nvim_command([[keepjumps normal! g`"zz]])
+        if window.last == buf_last then
+            set_to_last_place()
+        elseif buf_last - last > math.floor((window.last - window.first) / 2) - 1 then
+            set_to_last_place()
+            center_line()
         else
-            vim.api.nvim_command([[keepjumps normal! G'"<c-e>]])
+            vim.api.nvim_command([[keepjumps normal! G'"<C-e>]])
         end
     end
     if vim.fn.foldclosed('.') ~= -1 and Lastplace.options.open_folds then
-        vim.api.nvim_command([[normal! zvzz]])
+        center_line()
     end
 end
 
@@ -85,40 +101,39 @@ function Lastplace.lastplace_buf()
 
     local ignore_ft = Lastplace.options.ignore.ft
     if vim.list_contains(ignore_ft, vim.bo[bufnr].filetype) then
-        -- reset cursor to first line
-        vim.api.nvim_command([[normal! gg]])
+        reset_to_top()
         return
     end
 
-    -- If a line has already been specified on the command line, we are done
-    --   nvim file +num
+    ---If a line has already specified or on the cmdline, stop.
+    ---
+    ---Use case: `nvim file +num`
     if vim.fn.line('.') > 1 then
         return
     end
+
     set_cursor_position()
 end
 
----@param buffer integer
-function Lastplace.lastplace_ft(buffer)
+---@param bufnr integer
+function Lastplace.lastplace_ft(bufnr)
     local ignore_bt = Lastplace.options.ignore.bt
-    if vim.list_contains(ignore_bt, vim.bo[buffer].buftype) then
+    if vim.list_contains(ignore_bt, vim.bo[bufnr].buftype) then
         return
     end
 
     local ignore_ft = Lastplace.options.ignore.ft
-    if vim.list_contains(ignore_ft, vim.bo[buffer].filetype) then
-        -- reset cursor to first line
-        vim.api.nvim_command([[normal! gg]])
+    if vim.list_contains(ignore_ft, vim.bo[bufnr].filetype) then
+        reset_to_top()
         return
     end
 
-    -- If a line has already been set by the BufReadPost event or on the command
-    -- line, we are done.
+    ---If a line has already been set by the `BufReadPost` event or on the cmdline...
     if vim.fn.line('.') > 1 then
         return
     end
 
-    -- This shouldn't be reached but, better have it ;-)
+    ---Ideally this shouldn't be reached but better have it than not.
     set_cursor_position()
 end
 
